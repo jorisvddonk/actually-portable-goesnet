23a211c74bbbc7fdfd1c36a0b514bdb783ed383b
Joris van de Donk <jorisvddonk@gmail.com>
2024-03-20 23:07:43 +0100

    move things out into a compat.h header file

--- src/dl.c
+++ src/dl.c
@@ -21,22 +21,8 @@ const double MAX_OBJECTS_PER_STAR = 80;
 #include "libc/calls/calls.h"
 #include "libc/fmt/conv.h"
 #include "libc/sysv/consts/o.h"
+#include "compat.h"
 #include "brtl.h"
-
-// Compatibilty with Cosmopolitan libc:
-#define _read read
-#define _open open
-#define _argc argc
-#define _argv argv
-#define _close close
-#define strupr brtl_strupr
-#define random brtl_random
-#define srand brtl_srand
-
-// Various noctis defines
-#define M_PI       3.14159265358979323846   // pi
-#define M_PI_2     1.57079632679489661923   // pi/2
-#define M_PI_4     0.785398163397448309616  // pi/4
 
 //////////////////////////////////////////////////////////////////////////////
 
